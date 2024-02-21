aaa74d95ca1c742d2c2c663062313d63f3f73b2d
Arnold <arnold201991@gmail.com>
2024-02-21 15:25:19 +0100

    primera subida

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from 'antd';
+
+import './App.css'
+
+    const App = () => {
+      return (
+        <div>
+          <Button type="primary">¡Comprar!</Button>
+        </div>
+      );
+    }
+    
+
+export default App
