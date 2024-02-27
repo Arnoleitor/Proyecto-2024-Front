2338ec4a955397650f6aacd44acd813f92a4db09
Arnold <arnold201991@gmail.com>
2024-02-27 22:21:09 +0100

    registro y login ok desde front

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
-import { Form, Input, Button, Checkbox, message } from 'antd';
+import React, { useState } from 'react';
+import { Form, Input, Button, message, Modal } from 'antd';
+import axios from 'axios';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import Register from '../Auth/Register';
 
 const Login = () => {
-    const onFinish = async (values) => {
-      try {
-        const response = await axios.post('http://localhost:3000/auth/login', values);
-        console.log('Inicio de sesión correcto:', response.data);
-      } catch (error) {
-        console.error('Login failed:', error);
-        message.error("No existe ese correo o contraseña")
-      }
-    };
+  const [registerModalVisible, setRegisterModalVisible] = useState(false);
+
+  const showRegisterModal = () => {
+    setRegisterModalVisible(true);
+  };
+
+  const handleCancel = () => {
+    setRegisterModalVisible(false);
+  };
+
+  const onFinish = async (values) => {
+    try {
+      const response = await axios.post('http://localhost:3000/auth/login', values);
+      console.log('Inicio de sesión correcto:', response.data);
+    } catch (error) {
+      console.error('Login failed:', error);
+      message.error("No existe ese correo o contraseña");
+    }
+  };
 
   return (
     <Form
@@ -50,10 +62,20 @@ const Login = () => {
 
       <Form.Item>
         <Button type="primary" htmlType="submit" className="login-form-button">
-          Inicia sesion
+          Inicia sesión
         </Button>
-        <span style={{marginLeft:'1%'}}>O</span><a style={{marginLeft:'1%'}} href="../../components/Auth/Register.jsx">registrate!</a>
+        <span style={{marginLeft:'1%'}}>O</span>
+        <Button style={{marginLeft:'1%'}} type='primary' ghost onClick={showRegisterModal}>regístrate aquí!</Button>
       </Form.Item>
+
+      <Modal
+        title="Registro"
+        open={registerModalVisible}
+        onCancel={handleCancel}
+        footer={null}
+      >
+        <Register />
+      </Modal>
     </Form>
   );
 };
