9d0aa5f538ce8058987309d12c4e1bd89c8d8299
Arnold <arnold201991@gmail.com>
2024-03-08 10:07:52 +0100

    fix carpetas

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { store, persistor } from './app/store.js';
+import { store, persistor } from './store/store.js';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
