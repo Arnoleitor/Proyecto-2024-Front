89c9eec8491efa3bbbb8ce75ea576fd2db212630
Arnold <arnold201991@gmail.com>
2024-03-21 23:36:49 +0100

    fix

--- src/components/Localizacion/Localizacion.jsx
+++ src/components/Localizacion/Localizacion.jsx
@@ -5,6 +5,7 @@ const Localizacion = () => {
   const [location, setLocation] = useState(null);
   const [licence, setLicence] = useState(null);
   const [state, setState] = useState(null);
+  const [province, setProvince] = useState(null);
   const [postCode, setPostCode] = useState(null);
 
   useEffect(() => {
@@ -12,18 +13,18 @@ const Localizacion = () => {
       try {
         navigator.geolocation.getCurrentPosition(async (position) => {
           const { latitude, longitude } = position.coords;
-
           const apiUrl = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`;
-
           const response = await axios.get(apiUrl);
           const city =
-            response.data.address.city ||
+            response.data.address.town ||
+            response.data.address.province ||
             response.data.address.village ||
             response.data.address.state ||
             response.data.address.state_district ||
             response.data.address.isolated_dwelling;
-
+    
           setLocation(city);
+          setProvince(response.data.address.province);
           setLicence(response.data.licence);
           setState(response.data.address.state);
           setPostCode(response.data.address.postcode);
@@ -41,7 +42,7 @@ const Localizacion = () => {
     <div className='localizacion'>
       {location ? (
         <div>
-          <p style={{fontFamily:'monospace'}}>Tu ubicación actual: <strong>{location} - {state} - {postCode}</strong></p>
+          <p style={{fontFamily:'monospace'}}>Tu ubicación actual: <strong>{location} - {province} - {state} - {postCode}</strong></p>
           <p style={{fontFamily:'monospace'}}>Ubicación obtenida por: <strong>{licence}</strong></p>
         </div>
       ) : (
