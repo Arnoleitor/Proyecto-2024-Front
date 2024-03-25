88c12902036a7ff3cf39690a660d8f517cbb07be
Arnold <arnold201991@gmail.com>
2024-03-25 10:15:51 +0100

    respuesta para mensajes

--- src/pages/admin/componentes/ContactoMensaje.jsx
+++ src/pages/admin/componentes/ContactoMensaje.jsx
@@ -1,43 +1,56 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useFetch } from '../../../useHooks/useFetch';
-import { Divider, Table } from 'antd';
+import { Button, Divider, Table } from 'antd';
+import { MailOutlined } from '@ant-design/icons';
 
 const ContactoMensaje = () => {
-
   const [mensajes, setMensajes] = useState([]);
 
   const { data: responseMensajesData } = useFetch("http://localhost:3000/api/getcontacto");
+  
   useEffect(() => {
-      if (responseMensajesData) setMensajes(responseMensajesData);
+    if (responseMensajesData) setMensajes(responseMensajesData);
   }, [responseMensajesData]);
+
+  const handleReply = (email) => {
+    window.location.href = `mailto:${email}`;
+  };
 
   const columnsMensajesConctacto = [
     {
-        title: "Nombre",
-        dataIndex: "nombre",
-        key: "nombre",
+      title: "Nombre",
+      dataIndex: "nombre",
+      key: "nombre",
     },
     {
-        title: "Email",
-        dataIndex: "email",
-        key: "email",
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+      render: (email) => (
+        <a href={`mailto:${email}`} target="_blank" rel="noopener noreferrer">{email}</a>
+      )
     },
     {
       title: "Mensaje",
       dataIndex: "mensaje",
       key: "mensaje",
-  },
-];
+    },
+    {
+      title: "Responder",
+      key: "accion",
+      render: (_, record) => (
+        <Button icon={<MailOutlined/>} type='primary' onClick={() => handleReply(record.email)}></Button>
+      )
+    }
+  ];
 
-return (
-    <>
+  return (
     <div className='tablasAdmin'>
-        <h2>Mensajes de contacto</h2>
-        <Divider />
-        <Table dataSource={mensajes} columns={columnsMensajesConctacto} />
+      <h2>Mensajes de contacto</h2>
+      <Divider />
+      <Table dataSource={mensajes} columns={columnsMensajesConctacto} />
     </div>
-    </>
-  )
-}
+  );
+};
 
 export default ContactoMensaje;
